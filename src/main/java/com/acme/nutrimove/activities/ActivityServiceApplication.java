0d8bab21868094890b9c3052a9bc258062480b23
Nato0211 <nato0211@gmail.com>
2025-06-18 18:52:01 -0500

    feat: test update

--- src/main/java/com/acme/nutrimove/activities/ActivityServiceApplication.java
+++ src/main/java/com/acme/nutrimove/activities/ActivityServiceApplication.java
@@ -9,12 +9,30 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.sql.*;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.acme.nutrimove.activities.infrastructure.external")
 @EnableJpaAuditing
 public class ActivityServiceApplication {
     public static void main(String[] args) {
+        createDatabaseIfNotExists("jdbc:postgresql://localhost:5432/", "postgres", "renato", "nutrimove_activityservice");
+
         SpringApplication.run(ActivityServiceApplication.class, args);
+    }
+    private static void createDatabaseIfNotExists(String url, String user, String password, String dbName) {
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             Statement stmt = conn.createStatement()) {
+
+            ResultSet rs = stmt.executeQuery("SELECT 1 FROM pg_database WHERE datname = '" + dbName + "'");
+            if (!rs.next()) {
+                stmt.executeUpdate("CREATE DATABASE " + dbName);
+                System.out.println("Base de datos '" + dbName + "' creada correctamente.");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error verificando/creando base de datos: " + dbName, e);
+        }
     }
 }
